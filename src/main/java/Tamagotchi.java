51ac60f6129513760c7e54c90f3b1a8e57fb0237
Elissa Sosland and Reed Lambier <elissa@>
2016-04-26 14:07:42 -0700

    add passing test

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -1,9 +1,16 @@
 public class Tamagotchi {
+  private String mMyPet;
+
+  public Tamagotchi(String myPet) {
+    mMyPet = myPet;
+  }
+
+  public String getName() {
+    return mMyPet;
+  }
   /*int foodLevel = 10;
   int sleepLevel = 10;
   int activityLevel = 10;*/
 
-  public Tamagotchi(String myPet) {
 
-  }
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -3,17 +3,22 @@ import static org.junit.Assert.*;
 
 public class TamagotchiTest {
 
+  // @Test
+  // public void tamagotchi_instantiatesCorrectly() {
+  //   Tamagotchi myTamagotchi = new Tamagotchi("lil dragon");
+  //   assertEquals(true, myTamagotchi instanceof Tamagotchi);
+  // }
+
   @Test
   public void tamagotchi_instantiatesCorrectly() {
-    Tamagotchi myTamagotchi = new Tamagotchi("lil dragon");
-    assertEquals(false, myTamagotchi instanceof Tamagotchi);
-/*
-    Tamagotchi myPet = newTamagotchi("lil dragon");
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
     assertEquals("lil dragon", myPet.getName());
-    assertEquals(10, myPet.getFoodLevel());
-    assertEquals(10, myPet.getSleepLevel());
-    assertEquals(10, myPet.getActivityLevel());
-    */
+
+
+    // assertEquals(10, myPet.getFoodLevel());
+    // assertEquals(10, myPet.getSleepLevel());
+    // assertEquals(10, myPet.getActivityLevel());
+
   }
 
 }
