5c3c25d6d5f4df46f6cab934806cd5a706cff64e
Elissa Sosland and Reed Lambier <reed@>
2016-04-26 16:31:38 -0700

    add more tests and backend logic

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -1,16 +1,59 @@
 public class Tamagotchi {
   private String mMyPet;
+  private int mFoodLevel = 10;
+  private int mSleepLevel = 10;
+  private int mActivityLevel = 10;
 
   public Tamagotchi(String myPet) {
     mMyPet = myPet;
   }
 
   public String getName() {
+
     return mMyPet;
   }
-  /*int foodLevel = 10;
-  int sleepLevel = 10;
-  int activityLevel = 10;*/
+
+  public int getFoodLevel() {
+    return mFoodLevel;
+  }
+
+  public int getSleepLevel() {
+    return mSleepLevel;
+  }
+
+  public int getActivityLevel() {
+    return mActivityLevel;
+  }
+
+  public void clickFeed() {
+    mFoodLevel++;
+    mSleepLevel--;
+    mActivityLevel--;
+  }
+
+  public void clickSleep() {
+    mFoodLevel--;
+    mSleepLevel++;
+    mActivityLevel--;
+  }
+
+  public void clickActivityLevel() {
+    mFoodLevel--;
+    mSleepLevel--;
+    mActivityLevel++;
+  }
+
+  public boolean isAlive() {
+    if (mFoodLevel > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
+
 
 
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -13,12 +13,46 @@ public class TamagotchiTest {
   public void tamagotchi_instantiatesCorrectly() {
     Tamagotchi myPet = new Tamagotchi("lil dragon");
     assertEquals("lil dragon", myPet.getName());
-
-
-    // assertEquals(10, myPet.getFoodLevel());
-    // assertEquals(10, myPet.getSleepLevel());
-    // assertEquals(10, myPet.getActivityLevel());
-
+    assertEquals(10, myPet.getFoodLevel());
+    assertEquals(10, myPet.getSleepLevel());
+    assertEquals(10, myPet.getActivityLevel());
   }
+
+  @Test
+  public void clickFeed_SleepAndActivityDecrease() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    myPet.clickFeed();
+    assertEquals(9, myPet.getSleepLevel());
+    assertEquals(9, myPet.getActivityLevel());
+  }
+
+  @Test
+  public void clickSleep_FoodAndActivityDecrease() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    myPet.clickSleep();
+    assertEquals(9, myPet.getActivityLevel());
+    assertEquals(9, myPet.getFoodLevel());
+  }
+
+  @Test
+  public void clickActivityLevel_FoodAndSleepDecrease() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    myPet.clickActivityLevel();
+    assertEquals(9, myPet.getSleepLevel());
+    assertEquals(9, myPet.getFoodLevel());
+  }
+
+  @Test
+  public void isAlive_foodLevelAbove0_true() {
+    Tamagotchi  myPet = new Tamagotchi("lil dragon");
+    assertEquals(true, myPet.isAlive());
+  }
+
+  // @Test
+  // public void isAlive_foodBelow1_false() {
+  //   Tamagotchi myPet = new Tamagotchi("lil dragon");
+  //   myPet.setFoodLevel()
+  //   assertEquals(false, myPet.isAlive());
+  // }
 
 }
